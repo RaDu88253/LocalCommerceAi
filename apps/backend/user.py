59ef8dd3b4a8570d03045581661174303d8bb42d
Biancaaq <draghici.bianca1975@gmail.com>
2025-12-06 12:42:09 +0200

    Initial User Model + PassHash

---
+++ apps/backend/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    username = Column(String(255), unique=True, index=True)
+    first_name = Column(String(255), index=True)
+    last_name = Column(String(255), index=True)
+    hashed_password = Column(String(255), nullable=False)
+    phone_number = Column(String(50), unique=True, index=True)
+
+    def __repr__(self):
+        return f"<User(username='{self.username}', name='{self.first_name} {self.last_name}')>"
